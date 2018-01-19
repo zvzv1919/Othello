c4c8514b0f5d700029e8e40f5cb9816c1154319f
zxMacAir <zvzv1919@gmail.com>
2018-01-19 16:11:10 -0500

    1

--- src/Board.java
+++ src/Board.java
@@ -65,7 +65,6 @@ public class Board extends JPanel{
     }
 
     public void clicked(int x, int y) {
-        char zx;
         char newX = (char)(x / DEFAULT_GRID_SIZE + 'a' - 1);
         int newY = y / DEFAULT_GRID_SIZE;
 
